2cb74608315f886b37d848892e0ab7cc13d2b4c4
Matt Linaberry <matt.linaberry@we-ics.com>
2014-10-02 21:48:45 -0400

    Initial Commit

---
+++ Out of this World/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Matthew Linaberry on 10/2/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
